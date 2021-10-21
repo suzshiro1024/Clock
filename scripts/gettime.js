aa004374c380ab218bfff77ae3d3fd2dbaac0d5d
Tamamo-Dictus <tix131423113210@gmail.com>
2021-10-22 08:49:35 +0900

    add comment

--- scripts/gettime.js
+++ scripts/gettime.js
@@ -1,23 +1,26 @@
-const getTime = () => {
-    let time = new Date();
-    let year = time.getFullYear();
-    let month = time.getMonth() + 1;
-    let date = time.getDate();
-    let day = time.getDay();
-    const week = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
-    let weeksday = week[day];
-    let hour = time.getHours();
-    let min = time.getMinutes();
-    let sec = time.getSeconds();
+const get = () => {
+    let time = new Date();              // 現在日時情報を取得
+    let year = time.getFullYear();      // 年を取得
+    let month = time.getMonth() + 1;    // 月を取得
+    let date = time.getDate();          // 日を取得
+    let day = time.getDay();            // 曜日番号取得
+    // 曜日の表示のための配列
+    const weeks = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
+    let weekday = weeks[day];           // 曜日を確定
+    let hour = time.getHours();         // 時を取得
+    let min = time.getMinutes();        // 分を取得
+    let sec = time.getSeconds();        // 秒を取得
 
+    // 1桁の場合0を追加
+    // (ex)10-9-2021 => 10-09-2021
     month = month < 10 ? "0" + month : month;
     date = date < 10 ? "0" + date : date;
     hour = hour < 10 ? "0" + hour : hour;
     min = min < 10 ? "0" + min : min;
     sec = sec < 10 ? "0" + sec : sec;
 
-    let now_date = `${month}-${date}-${year} (${weeksday})`;
-    let now_time = `${hour}:${min}:${sec}`;
+    let now_date = `${month}-${date}-${year} (${weekday})`; // mm-dd-yyyy (day)
+    let now_time = `${hour}:${min}:${sec}`;                 // hh:mm:ss
     let place = `Japan Tokyo (GMT+9.00)`;
 
     document.querySelector(".date").innerText = now_date;
@@ -25,4 +28,4 @@ const getTime = () => {
     document.querySelector(".place").innerText = place;
 }
 
-setInterval(getTime,1000);
+setInterval(get,500);
