088712ff1b626e62eb9ad69f35dd71b4a83ad191
Tamamo-Dictus <tix131423113210@gmail.com>
2021-10-25 13:19:01 +0900

    unify blanks

--- scripts/location.js
+++ scripts/location.js
@@ -1,5 +1,5 @@
 const getLocation = () => {
-    if( navigator.geolocation){
+    if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(
 
             // 成功した場合に呼び出す
@@ -8,7 +8,7 @@ const getLocation = () => {
 
                 var lat = data.latitude;    // 緯度
                 var lng = data.longitude;   // 経度
-                var place;
+                var place;                  // 場所を格納
 
                 // 国土地理院の逆ジオコーディングAPIを利用
                 var url = "https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress"
@@ -16,23 +16,23 @@ const getLocation = () => {
                 const qs = new URLSearchParams(params);                 // URLの末尾につけられるように関数を通して変換
 
                 // fetch APIを使ってresponceオブジェクトの形で逆ジオコーディングAPIの結果を取得
-                fetch(`${url}?${qs}`).then(function (responce){
+                fetch(`${url}?${qs}`).then(function(responce){
                     return responce.json();                             // json形式に変換
 
                 // 成功時に実行
-                }).then(function (data){
+                }).then(function(data){
                     var num = data.results.muniCd;                      // json形式のデータから必要な部分を抽出
                     var array_data = MUNI_ARRAY[num];                   // muni.jsの配列から地名情報を取得
                     var pref_array = array_data.split(',');             // ","で区切る
                     var pref = pref_array[1];                           // 県名
-                    var muni = pref_array[3];                           // 市町村名
+                    var muni = pref_array[3].replace('　',' ');         // 市町村名(全角空白を半角空白へ置換する)
                     place = `${pref} ${muni}`;                          // 実際に表示する形式
                     document.querySelector(".place").innerText = place; // 時計に表示
                 });
             },
 
             // 失敗した場合に呼び出す
-            function (error) {
+            function(error){
                 // error.code
                 // 0:Unknown_Error
                 // 1:Permission_Denied
