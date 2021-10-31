efa1db325ff54837186976e5b7d3421c7727fbad
Tamamo-Dictus <tix131423113210@gmail.com>
2021-10-31 22:51:46 +0900

    temporary saving

---
+++ scripts/stopwatch.js
@@ -0,0 +1,8 @@
+const stopwatch = () => {
+    'use.strict';
+
+    var stopwatch = document.getElementById('stopwatch');
+    var start = document.getElementById('start');
+    var stop = document.getElementById('stop');
+    var reset = document.getElementById('reset');
+}
