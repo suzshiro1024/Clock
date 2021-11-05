09f46803d553d07a345748f890f6b92698fad7cb
Tamamo-Dictus <tix131423113210@gmail.com>
2021-11-05 22:29:44 +0900

    edit commentout

--- scripts/location.js
+++ scripts/location.js
@@ -2,42 +2,42 @@ const getLocation = () => {
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(
 
-            // 成功した場合に呼び出す
+            //成功した場合に呼び出す
             function success(position){
-                var data = position.coords; // 取得したデータを投入
+                var data = position.coords; //取得したデータを投入
 
-                var lat = data.latitude;    // 緯度
-                var lng = data.longitude;   // 経度
-                var place;                  // 場所を格納
+                var lat = data.latitude;    //緯度
+                var lng = data.longitude;   //経度
+                var place;                  //場所を格納
 
                 // 国土地理院の逆ジオコーディングAPIを利用
                 var url = "https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress"
-                const params = {lon: lng, lat: lat};                    // パラメータにGeolocation APIで取得した緯度経度情報を入力
-                const qs = new URLSearchParams(params);                 // URLの末尾につけられるように関数を通して変換
+                const params = {lon: lng, lat: lat};                    //パラメータにGeolocation APIで取得した緯度経度情報を入力
+                const qs = new URLSearchParams(params);                 //URLの末尾につけられるように関数を通して変換
 
-                // fetch APIを使ってresponceオブジェクトの形で逆ジオコーディングAPIの結果を取得
+                //fetch APIを使ってresponceオブジェクトの形で逆ジオコーディングAPIの結果を取得
                 fetch(`${url}?${qs}`).then(function(responce){
-                    return responce.json();                             // json形式に変換
+                    return responce.json();                             //json形式に変換
 
-                // 成功時に実行
+                //成功時に実行
                 }).then(function(data){
-                    var num = data.results.muniCd;                      // json形式のデータから必要な部分を抽出
-                    var array_data = MUNI_ARRAY[num];                   // muni.jsの配列から地名情報を取得
-                    var pref_array = array_data.split(',');             // ","で区切る
-                    var pref = pref_array[1];                           // 県名
-                    var muni = pref_array[3].replace('　',' ');         // 市町村名(全角空白を半角空白へ置換する)
-                    place = `${pref} ${muni}`;                          // 実際に表示する形式
-                    document.querySelector(".place").innerText = place; // 時計に表示
+                    var num = data.results.muniCd;                      //json形式のデータから必要な部分を抽出
+                    var array_data = MUNI_ARRAY[num];                   //muni.jsの配列から地名情報を取得
+                    var pref_array = array_data.split(',');             //","で区切る
+                    var pref = pref_array[1];                           //県名
+                    var muni = pref_array[3].replace('　',' ');         //市町村名(全角空白を半角空白へ置換する)
+                    place = `${pref} ${muni}`;                          //実際に表示する形式
+                    document.querySelector(".place").innerText = place; //時計に表示
                 });
             },
 
-            // 失敗した場合に呼び出す
+            //失敗した場合に呼び出す
             function(error){
-                // error.code
-                // 0:Unknown_Error
-                // 1:Permission_Denied
-                // 2:Position_Unavailable
-                // 3:Timeout
+                //error.code
+                //0:Unknown_Error
+                //1:Permission_Denied
+                //2:Position_Unavailable
+                //3:Timeout
 
                 var info = [
                     "Unknown Error / 原因不明エラー",
@@ -53,7 +53,7 @@ const getLocation = () => {
                 document.querySelector(".place").innerText = message;
             },
 
-            // オプション
+            //オプション
             {
                 "enableHighAccuracy": false,
                 "timeout": 8000,
--- scripts/stopwatch.js
+++ scripts/stopwatch.js
@@ -6,18 +6,18 @@ const stopwatch = () => {
     var stop      = document.getElementById('stop');        //STOPボタン
     var reset     = document.getElementById('reset');       //RESETボタン
 
-    var startTime;      // 開始時間
-    var lapsedTime = 0; // 経過時間(ミリ秒)
-    var stopwatchId;    // Id
-    var mem = 0;        // ストップ後、リセットなしでは0秒に戻らないようにする
-    var flag = false;   // 計測中フラグ
+    var startTime;      //開始時間
+    var lapsedTime = 0; //経過時間(ミリ秒)
+    var stopwatchId;    //Id
+    var mem = 0;        //ストップ後、リセットなしでは0秒に戻らないようにする
+    var flag = false;   //計測中フラグ
 
     function transform(){
-        var min = Math.floor(lapsedTime / 60000);           // 60000で割ると分が得られる
-        var sec = Math.floor(lapsedTime % 60000 / 1000);    // 1000で割ると秒が得られる
-        var msc = Math.floor(lapsedTime % 1000);            // 1000で割った余りはミリ秒
+        var min = Math.floor(lapsedTime / 60000);           //60000で割ると分が得られる
+        var sec = Math.floor(lapsedTime % 60000 / 1000);    //1000で割ると秒が得られる
+        var msc = Math.floor(lapsedTime % 1000);            //1000で割った余りはミリ秒
 
-        // 9 => 09という形で調整する
+        //9 => 09という形で調整する
         min = min < 10 ? '0' + min : min;
         sec = sec < 10 ? '0' + sec : sec;
         msc = msc < 10 ? '00' + msc : msc < 100 ? '0' + msc : msc;
@@ -26,40 +26,40 @@ const stopwatch = () => {
     }
 
     function count(){
-        // setTimeoutの返り値を代入
+        //setTimeoutの返り値を代入
         stopwatchId = setTimeout(function(){
-            // 経過時間 = 現在時刻 - 開始時刻 + 途中で止めた時刻(あれば)
+            //経過時間 = 現在時刻 - 開始時刻 + 途中で止めた時刻(あれば)
             lapsedTime = Date.now() - startTime + mem;
             transform();
 
-            // 再帰
+            //再帰
             count();
-        // msが飛ばないように調整
+        //msが飛ばないように調整
         },10);
     }
 
     //STARTボタンイベント
     start.addEventListener('click',function(){
         if(flag == false){
-        startTime = Date.now();                 // 現在時刻を代入
-        count();                                // 計測開始
-        flag = true;                            // フラグを立てる
+        startTime = Date.now();                 //現在時刻を代入
+        count();                                //計測開始
+        flag = true;                            //フラグを立てる
         }
     });
 
     //STOPボタンイベント
     stop.addEventListener('click',function(){
-        clearTimeout(stopwatchId);              // 計測停止
+        clearTimeout(stopwatchId);              //計測停止
         if(flag == true){
-            mem += Date.now() - startTime;      // 計測再開に備えて経過時間を記憶
-            flag = false;                       // フラグをおろす
+            mem += Date.now() - startTime;      //計測再開に備えて経過時間を記憶
+            flag = false;                       //フラグをおろす
         }
     });
 
     //RESETボタンイベント
     reset.addEventListener('click',function(){
-        lapsedTime = 0;                         // 経過時間のリセット
-        mem = 0;                                // メモリもリセット
+        lapsedTime = 0;                         //経過時間のリセット
+        mem = 0;                                //メモリもリセット
         transform();
     });
 }
