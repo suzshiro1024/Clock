389d9920fbaaabab8aca34e1d376e000f098b5d1
Tamamo-Dictus <tix131423113210@gmail.com>
2021-11-08 15:08:34 +0900

    change timekeeping method.

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,23 +1,24 @@
 const timer = () => {
-    'use strict';
+  	'use strict';
 
-    const timer = document.getElementById('timer');   //タイマーそのもの
+  	const timer = document.getElementById('timer');   //タイマーそのもの
     const start = document.getElementById('start');   //STARTボタン
     const stop  = document.getElementById('stop');    //STOPボタン
     const reset = document.getElementById('reset');   //RESETボタン
     const set   = document.getElementById('timeset'); //TIMER SETボタン
 
-    let time = 0;           //設定時間
     let timerId;            //Id
-    let startTime;          //開始時間
+    let remainingTime;      //残り時間
+    let startTime = 0;      //開始時間
+    let endTime;            //終了時間
     let mem = 0;            //ストップ後、リセットなしでは0秒に戻らないようにするメモリ
     let flag = false;       //計測中フラグ
     let timeout = false;    //時間切れ確認
 
     function transform(){
-        let hour= Math.floor(time / 3600000);                   //3600000で割ると時が得られる
-        let min = Math.floor(time % 3600000 / 60000);           //60000で割ると分が得られる
-        let sec = Math.floor(time % 3600000 % 60000 / 1000);    //1000で割ると秒が得られる
+        let hour= Math.floor(remainingTime / 3600000);                   //3600000で割ると時が得られる
+        let min = Math.floor(remainingTime % 3600000 / 60000);           //60000で割ると分が得られる
+        let sec = Math.floor(remainingTime % 3600000 % 60000 / 1000);    //1000で割ると秒が得られる
 
         //9 => 09という形で調整する
         hour= hour< 10 ? '0' + hour:hour;
@@ -30,15 +31,18 @@ const timer = () => {
     function count(){
         //setTimeoutの返り値を代入
         timerId = setTimeout(function(){
-            //時間を10ミリ秒減らす
-            time -= 10;
+            //残り時間を計算
+            remainingTime = endTime - Date.now();
             //整形
             transform();
-            //再帰
+						//再帰
             count();
-            if(time == 0){
+            if(remainingTime <= 0){
                 //強制停止する
                 clearTimeout(timerId);
+								//時間を0にして表示
+								remainingTime = 0;
+								transform();
                 //時間切れになったら点滅して合図を出す
                 timeout = true;
                 timer.classList.add("timeout");
@@ -49,8 +53,9 @@ const timer = () => {
 
     //STARTボタンイベント
     start.addEventListener('click',function(){
-        if(flag == false && time != 0){
+        if(flag == false && remainingTime != 0){
             startTime = Date.now();
+            endTime = Date.now() + remainingTime;
             count();        //計測開始
             flag = true;    //フラグを立てる
         }
@@ -58,10 +63,10 @@ const timer = () => {
 
     //STOPボタンイベント
     stop.addEventListener('click',function(){
-        clearTimeout(timerId);  //計測停止
+        clearTimeout(timerId);  	//計測停止
         if(flag == true){
-            mem += time;        //計測再開に備えて時間を記憶
-            flag = false;       //フラグをおろす
+            mem += remainingTime;	//計測再開に備えて時間を記憶
+            flag = false;       	//フラグをおろす
         }
         if(timeout == true){
             //STOPを押すことで時間切れ表示のための点滅を解除
@@ -73,9 +78,9 @@ const timer = () => {
     //RESETボタンイベント
     reset.addEventListener('click',function(){
         if(flag == false){
-            time = 0;       //経過時間のリセット
-            mem = 0;        //メモリもリセット
-            transform();    //整形
+            remainingTime = 0;	//経過時間のリセット
+            mem = 0;        		//メモリもリセット
+            transform();    		//整形
         }
     });
 
@@ -88,7 +93,7 @@ const timer = () => {
             // 分割
             var set  = user.split(':');
             // ミリ秒変換して代入
-            time = Number(set[0])*3600000 + Number(set[1])*60000 + Number(set[2])*1000;
+            remainingTime = Number(set[0])*3600000 + Number(set[1])*60000 + Number(set[2])*1000;
             transform();    // 整形
         }
     });
